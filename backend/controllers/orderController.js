ce62f6971d39a4584794abddcfae615afffa073d
Smit Satasiya <152951890+SmitSatasiya@users.noreply.github.com>
2025-03-21 13:20:37 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
 
-  const frontend_url = "http://localhost:5173";
+  // const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-del-frontend-i13g.onrender.com/";
 
   try {
     const newOrder = new orderModel({
